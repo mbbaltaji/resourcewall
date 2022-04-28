1312b97d54b0a0154cbd40e343b29a0ff716b66e
DinoPranjic <dinopranjic92@gmail.com>
2022-04-28 17:02:03 +0000

    basic template to display resources

--- public/scripts/resources.js
+++ public/scripts/resources.js
@@ -26,18 +26,30 @@ const renderResources = (data) => {
 }
 */
 const createResourceItem = function (resource) {
-  return $("<li>").text(`${resource.url} - ${resource.title} - ${resource.description} - ${resource.created_at}`);
+  //return $("<li>").text(`${resource.url} - ${resource.title} - ${resource.description} - ${resource.created_at}`);
+  const $resourceCard =
+  $(`<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${resource.title}</h5>
+    <p class="card-text">${resource.description}</p>
+    <a href="${resource.url}" class="btn btn-primary">${resource.title}</a>
+  </div>
+</div>`);
+
+  return $resourceCard;
 };
 
+
 $(document).ready(() => {
-  const container = $("#resource-container");
+  const $container = $("#resource-container");
     $.ajax({
     url: "/api/resources",
     method: "GET",
     dataType: "json",
     success: (data) => {
       const resourceElements = data.resources.map(createResourceItem);
-      container.append(resourceElements);
+      $container.append(resourceElements);
     },
     error: (xhr, status, errorMessage) => {
       console.log("error recieved", status, errorMessage);
