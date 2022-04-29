f4454b07649c08549d83937e09233b4ab5e9be3d
DinoPranjic <dinopranjic92@gmail.com>
2022-04-29 00:23:03 +0000

    add category icons to dynamically appear

--- db/seeds/02_resources.sql
+++ db/seeds/02_resources.sql
@@ -1,7 +1,9 @@
 INSERT INTO resources(user_id, url, title, description, category, created_at, deleted_on)
-VALUES(1,'https://www.codecademy.com/', 'codecademy', 'learn how to do hard things when they are too hard to figure out alone', 'coding', now(), NULL),
-(2,'https://www.khanacademy.org/', 'Khan Academy', 'learn how to do hard things when they are too hard to figure out alone', 'math', now(), NULL),
-(1,'https://www.lighthouselabs.ca/', 'Lighthouse Lab', 'learn how to do hard things when video tutorials arent enough and you need teachers and mentors', 'coding', now(), NULL),
-(2,'https://www.github.com/', 'Git Hub', 'save coding projects', 'web development', now(), NULL),
-(1,'https://www.w3schools.com/', 'w3schools', 'learn how to do hard things when they are too hard to figure out alone', 'coding', now(), NULL)
+VALUES
+(1,'https://www.codecademy.com/', 'codecademy', 'learn how to do hard things when they are too hard to figure out alone', 'coding', now()::date, NULL),
+(1,'https://www.khanacademy.org/', 'Khan Academy', 'learn how to do hard things when they are too hard to figure out alone', 'educational', now()::date, NULL),
+(1,'https://www.lighthouselabs.ca/', 'Lighthouse Lab', 'learn how to do hard things when video tutorials arent enough and you need teachers and mentors', 'coding', now()::date, NULL),
+(1,'https://www.deviantart.com/', 'Deviant Art', 'The largest Online Art Gallery and Community', 'art', now()::date, NULL),
+(1,'https://www.w3schools.com/', 'w3schools', 'learn how to do hard things when they are too hard to figure out alone', 'coding', now()::date, NULL),
+(1,'https://www.youtube.com/watch?v=mhDJNfV7hjk', 'Gordan Ramsey Cooking', 'While a lot of us are remaining indoors, here are a few quick, simple and cheap recipes to follow to learn.', 'cooking', now()::date, NULL)
 ;
--- public/scripts/resources.js
+++ public/scripts/resources.js
@@ -25,13 +25,28 @@ const renderResources = (data) => {
   }
 }
 */
+const categories = {
+  sports: "fa-solid fa-basketball",
+  educational: "fa-solid fa-graduation-cap",
+  art: "fa-solid fa-palette",
+  coding: "fa-solid fa-code",
+  cooking: "fa-solid fa-utensils",
+  music: "fa-solid fa-music",
+  travel: "fa-solid fa-bus"
+};
+
 const createResourceItem = function (resource) {
+  const icon = categories[resource.category];
+  console.log(icon);
   //return $("<li>").text(`${resource.url} - ${resource.title} - ${resource.description} - ${resource.created_at}`);
   const img = `http://api.pagepeeker.com/v2/thumbs.php?size=m&code={code}&refresh={refresh}&wait={wait}&url=${resource.url}`;
   const $resourceCard = $(`<div class="card" style="width: 18rem;">
   <img class="card-img-top" src="${img}" alt="Card image cap">
   <div class="card-body">
+  <div class="title">
     <h5 class="card-title">${resource.title}</h5>
+    <i class="${icon}"></i>
+  </div>
     <p class="card-text">${resource.description}</p>
     <a href="${resource.url}" class="btn btn-primary">${resource.title}</a>
   </div>
