26ea0498066636491c3b37072731faf4f511f4ba
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-04-29 18:04:06 +0000

    feat: add search functionality to web app

--- routes/resources.js
+++ routes/resources.js
@@ -10,9 +10,27 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+
+    console.log('query: ', req.body.searchQuery);
     let query = `SELECT * FROM resources`;
     console.log(query);
     db.query(query)
+      .then(data => {
+        const resources = data.rows;
+        res.json({ resources });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    // query to find resources by title
+    let query = `SELECT * FROM resources WHERE title = $1`;
+    // req.body.searchQuery contains the value from the search input field
+    db.query(query, [req.body.searchQuery])
       .then(data => {
         const resources = data.rows;
         res.json({ resources });
